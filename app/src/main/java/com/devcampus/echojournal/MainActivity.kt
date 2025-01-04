942eb210f4aaef9d5ae36de995504a399429d9c9
hgarciaalberto <hgarcia.alberto@gmail.com>
2025-01-04 18:08:59 +0100

    Add Splash Screen

--- app/src/main/java/com/devcampus/echojournal/MainActivity.kt
+++ app/src/main/java/com/devcampus/echojournal/MainActivity.kt
@@ -11,12 +11,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.devcampus.echojournal.ui.theme.EchoJournalTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+
+        installSplashScreen()
         enableEdgeToEdge()
+
+        super.onCreate(savedInstanceState)
+
         setContent {
             EchoJournalTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
